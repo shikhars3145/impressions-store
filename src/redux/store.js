551b53de8cd1cf657e299f5b4cf3700df78b9144
xANARKYx <shikhars3145@gmail.com>
2020-08-04 04:45:43 +0530

    used redux-persist library to persist state of cart and user

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,15 @@
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
+import { persistStore } from 'redux-persist';
 
 import rootReducer from './rootReducer';
 
 const middlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
-
 //store is passed as a parameter to Provider in index.js
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+//persistor is passed to
+export const persistor = persistStore(store);
+
 export default store;
